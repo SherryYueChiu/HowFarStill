989a39e5736ac84f410c61a766be194eb41f6e45
Sherry <sherryyue.c@protonmail.com>
2021-05-07 12:17:43 +0800

    create HowfarStill

---
+++ sw-precache-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    staticFileGlobs: [
+        './',
+        './app.js',
+        './style.css',
+        './index.html'
+    ],
+    runtimeCaching: [{
+        urlPattern: /.*/,
+        handler: 'networkFirst'
+    }],
+    swFile: 'service-worker.js'
+};
